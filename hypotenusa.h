0642980ec8564e667563afe0794c03cdc3700f33
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 13:39:49 +0300

    Create hypotenusa.h
    initial .h header file for program calculating hypotenusa (language Finnish)

---
+++ hypotenusa.h
@@ -0,0 +1,11 @@
+/* oma header: hypotenusa.h */
+
+/* muuttujat */
+double lopputulos = 0;
+
+/* tyypit */
+#define LUKUVIRHE 9999
+
+/* funktion määritelmät */
+void tarkistus( );
+double laske_hypotenuusa( );
